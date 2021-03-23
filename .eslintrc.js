61d7be696fdead31562bfeb3372427ef0d235b9e
tiaotiao86 <tiaotiao8602@126.com>
2021-03-23 17:06:46 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        '@vue/standard'
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
